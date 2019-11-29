27fc62bd573c77e37f044e7886ccefd251ca0e36
Finn Thye <piqueen@gmail.com>
2019-11-28 22:47:06 -0700

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,17 @@
 require_relative '../config/environment.rb'
 
+amelie = Artist.new("Amelie", 6)
+hazel = Artist.new("Hazel", 4)
+mama = Artist.new("Mama", 42)
+
+shocker = Gallery.new("Shocker", "Denver")
+freezer = Gallery.new("Freezer", "Anchorage")
+shindig = Gallery.new("Shindig Joint", "Boulder")
+
+crayola = Painting.new("Crayola", 325.00, amelie, shindig)
+ecstacy = Painting.new("Ecstacy", 6709.00, hazel, shindig)
+sarc = Painting.new("Sarcophaguses", 980986.00, hazel, freezer)
+
 
 
 
